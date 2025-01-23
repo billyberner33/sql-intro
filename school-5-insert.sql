709a48db7a5b15d4a7dbd1bf5b3da76692c86dda
Billy Berner <billy.berner@kellogg.northwestern.edu>
2025-01-23 00:55:33 +0000

    wrote some DB code

---
+++ school-5-insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO students (
+first_name,
+last_name,
+email
+) VALUES (
+    "Billy",
+    "Berner",
+    "billy.berner@kellogg.northwestern.edu"
+);
+INSERT INTO students (
+first_name,
+last_name,
+email
+) VALUES (
+    "Billy",
+    "Berner",
+    "berner.billy@gmail.com"
+);
